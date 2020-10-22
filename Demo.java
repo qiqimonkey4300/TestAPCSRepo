1a917d4efafd877f747b609d115631eda20cb8c2
qiqimonkey4300 <yueqi04@gmail.com>
2020-10-22 16:51:48 -0400

    I tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -5,6 +5,18 @@ public class Demo {
     } else {
       printLoop(5);
     }
+    int[] zero = {};
+    int[] one = {1};
+    int[] two = {1, 2};
+    int[] three = {1, 2, 3};
+    int[] four = {1, 2, 3, 4};
+    int[][] d = new int[5][];
+    d[0] = zero;
+    d[1] = one;
+    d[2] = two;
+    d[3] = three;
+    d[4] = four;
+    System.out.println(arrayDeepToString(d));
   }
   public static void printLoop(int n) {
     for (int i = 1; i <= n; i++) {
@@ -13,5 +25,27 @@ public class Demo {
       }
       System.out.println();
     }
+  }
+  public static String arrToString(int[] arr) {
+    String x = "{";
+    for (int i = 0; i < arr.length - 1; i++) {
+      x = x + String.valueOf(arr[i]) + ", ";
+    }
+    if (arr.length > 0) {
+      x = x + String.valueOf(arr[arr.length - 1]);
+    }
+    x = x + "}";
+    return x;
+  }
+  public static String arrayDeepToString(int[][] arr) {
+    String y = "{";
+    for (int i = 0; i < arr.length - 1; i++) {
+      y = y + arrToString(arr[i]) + ", ";
+    }
+    if (arr.length > 0) {
+      y = y + String.valueOf(arrToString(arr[arr.length - 1]));
+    }
+    y = y + "}";
+    return y;
   }
 }

addee23bc80365f3eac5500db704d0ec5990f0b1
qiqimonkey4300 <yueqi04@gmail.com>
2020-10-22 17:57:14 -0400

    I tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -5,20 +5,20 @@ public class Demo {
     } else {
       printLoop(5);
     }
-    int[] zero = {};
-    int[] one = {1};
-    int[] two = {1, 2};
-    int[] three = {1, 2, 3};
-    int[] four = {1, 2, 3, 4};
-    int[][] d = new int[5][];
-    d[0] = zero;
-    d[1] = one;
-    d[2] = two;
-    d[3] = three;
-    d[4] = four;
-    System.out.println(arrayDeepToString(d).replace("}, ","},\n "));
-    System.out.println(arrayDeepToString(create2DArrayRandomized(6,4,4)).replace("}, ","},\n "));
-    System.out.println(arrayDeepToString(create2DArrayRandomized(4,10,100)).replace("}, ","},\n "));
+//    int[] zero = {};
+//    int[] one = {1};
+//    int[] two = {1, 2};
+//    int[] three = {1, 2, 3};
+//    int[] four = {1, 2, 3, 4};
+//    int[][] d = new int[5][];
+//    d[0] = zero;
+//    d[1] = one;
+//    d[2] = two;
+//    d[3] = three;
+//    d[4] = four;
+//    System.out.println(arrayDeepToString(d).replace("}, ","},\n "));
+//    System.out.println(arrayDeepToString(create2DArrayRandomized(6,4,4)).replace("}, ","},\n "));
+//    System.out.println(arrayDeepToString(create2DArrayRandomized(4,10,100)).replace("}, ","},\n "));
   }
   public static void printLoop(int n) {
     for (int i = 1; i <= n; i++) {
